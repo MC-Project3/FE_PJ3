c360f9fefcb251df9d650398fbd5faa8b7fffe35
HONGJAE <ghwo998@naver.com>
2023-05-19 11:44:48 +0900

    feat:Result.js 코드 완성

--- src/App.js
+++ src/App.js
@@ -8,10 +8,13 @@ import classes from './App.module.css';
 import Layout from './layout/Layout';
 import HeaderTitle from './components/HeaderTitle';
 
+const API_ENDPOINT = 'https://m7kq45fu7g.execute-api.ca-central-1.amazonaws.com/nba'
+
 function App() {
   const [selectedPlayer, setSelectedPlayer] = useState();
   const [selectedTeam, setSelectedTeam] = useState();
-  const [nbaResult, setnbaResult] = useState();
+  const [nbaResultBase64Image, setNbaResultBase64Image] = useState();
+
   const playerSelectHandler = (player) => {
     console.log(player);
     setSelectedPlayer(player.id === selectedPlayer?.id ? null : player)
@@ -23,8 +26,13 @@ function App() {
 
   const mergeClickHandler = () => {         //버튼 활성화 -> 클릭 시 Result에 selectedPlayer와 selectedTeam이 넘어가야한다.
     console.log("mergeButton is working");
-    //selectedPlayer, selectedTeam;
     // TODO: merge API 호출 후, 리턴값을 state로 저장
+    fetch(`${API_ENDPOINT}/merge?playerId=${selectedPlayer.id}&teamId=${selectedTeam.id}`)
+      .then(res => res.text())  // data:image/png;base64,iVBOR~~~
+      .then(base64 => {
+        console.log(base64);
+        setNbaResultBase64Image(base64);
+      })
   }
 
   return (
@@ -32,7 +40,7 @@ function App() {
       <SearchPlayer selectedPlayer={selectedPlayer} onPlayerSelect={playerSelectHandler} />
       <SearchTeam selectedTeam={selectedTeam} onTeamSelect={teamSelectHandler} />
       <MergeButton disabled={!(selectedPlayer && selectedTeam)} onClick={mergeClickHandler} />
-      <Result player={selectedPlayer} team={selectedTeam} />
+      <Result image={nbaResultBase64Image} />
     </Layout>
   );
 }
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,15 +1,14 @@
 
 
 export default function Result(props) {
-
-    //props.player;
-    //props.team;
-
-
-    return <article style={{ backgroundColor: 'rgb(0,0,100)' }}>
-        <h2>합성된 이미지가 표시됩니다</h2>
-
-
-
-    </article>
+    //합성된 이미지가 없으면 <h2>태그가 활성화 <img> 태그가 비활성화
+    return (
+        <article style={{ backgroundColor: 'rgb(0,0,100)' }}>
+            {
+                props.image ?
+                    <img src={props.image} /> :
+                    <h2>합성된 이미지가 표시됩니다</h2>
+            }
+        </article>
+    )
 }
