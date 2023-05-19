13f8b740d548f9cef25c70041f22dbac98661462
HONGJAE <ghwo998@naver.com>
2023-05-19 13:23:59 +0900

    feat(Result):결과 이미지 layout 만듬

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,13 +1,13 @@
-
+import classes from './Result.module.css'
 
 export default function Result(props) {
     //합성된 이미지가 없으면 <h2>태그가 활성화 <img> 태그가 비활성화
     return (
-        <article style={{ backgroundColor: 'rgb(0,0,100)' }}>
+        <article className={classes.container}>
             {
                 props.image ?
-                    <img src={props.image} /> :
-                    <h2>합성된 이미지가 표시됩니다</h2>
+                    <img className={classes.image} src={props.image} /> :
+                    <h2 >합성된 이미지가 표시됩니다</h2>
             }
         </article>
     )
